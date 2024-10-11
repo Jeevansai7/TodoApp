4bd75a903a941522b07333b5942f98bd5903e6eb
JeevanSai <jeevansaigutha@gmail.com>
2024-10-11 17:09:22 +0530

    j

--- server/src/services/fetchTodoTasks.js
+++ server/src/services/fetchTodoTasks.js
@@ -6,6 +6,7 @@ exports.fetchTasks = async (req) => {
       const pool=await dBConnection()
       console.log('service called',pool)
       result = await pool.query(`SELECT id,task,updated_on FROM todo`)
+      console.log(await pool.query(`SELECT id,task,updated_on FROM todo`))
       return  result
    }
    catch (e) {
