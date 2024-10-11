d03daa72f2dce31185ff22c312a468d3c34b3621
JeevanSai <jeevansaigutha@gmail.com>
2024-10-11 18:33:33 +0530

    CHANGES IN PORT

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,12 @@ const cors=require('cors')
 require('dotenv').config()
 const todoRoutes=require('./routes/routes')
 const app = express()
-const port = process.env.PORT || 4000;
+
 app.use(cors())
 app.use(express.json());
 
 app.use('/todo',todoRoutes)
 
-app.listen(port,()=>{
-    console.log(`server started on port ${port}`)
+app.listen(3001,()=>{
+    console.log("server started on port 3001")
 })
