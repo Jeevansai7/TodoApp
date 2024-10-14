54026f212774082247f36daa567005463dcabf76
JeevanSai <jeevansaigutha@gmail.com>
2024-10-14 11:12:07 +0530

    updated code

--- server/src/services/fetchTodoTasks.js
+++ server/src/services/fetchTodoTasks.js
@@ -15,7 +15,6 @@ exports.fetchTasks = async (req) => {
 
    let result = -1;
    try {
-      const pool=await dBConnection()
       console.log('service called',pool)
       result = await pool.query(`SELECT id,task,updated_on FROM todo`)
       console.log(await pool.query(`SELECT id,task,updated_on FROM todo`))
