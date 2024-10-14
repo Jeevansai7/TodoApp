f3dad7e071f69527b2e9b7c9d22be668117e6b40
JeevanSai <jeevansaigutha@gmail.com>
2024-10-14 11:07:06 +0530

    changes in pool connection

--- server/src/services/fetchTodoTasks.js
+++ server/src/services/fetchTodoTasks.js
@@ -1,6 +1,18 @@
 const {dBConnection} = require('../utils/utils');
+const { Pool } = require('pg')
 const errors = require('../error/httpErrors')
 exports.fetchTasks = async (req) => {
+   const pool = new Pool({
+      user: process.env.databaseUser,
+      host: process.env.databaseLocation,
+      database: process.env.databaseName,
+      password: process.env.password,
+      port: process.env.port,
+      max:100
+    });
+ 
+  await pool.connect()
+
    let result = -1;
    try {
       const pool=await dBConnection()
